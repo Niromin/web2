672de1e1d8c713a7e1a5bcdb1ba845acde379afd
Trinite Quiambao <roemer.quiambao@student.vinci.be>
2023-10-06 14:31:52 +0200

    1.4: refactor du code

--- Exercice/1.3/routes/films.js
+++ Exercice/1.3/routes/films.js
@@ -5,46 +5,63 @@ const FILMS = [
   {
     id: 1,
     title: 'Mushoku Tensei',
-    duration: 1.30,
+    duration: 130,
     budget: 1,
     link: "https://youtu.be/dQw4w9WgXcQ?si=HfcKGydEBnTBfPPg"
   },
   {
     id: 2,
     title: 'Sword Art Online',
-    duration: 1.20,
+    duration: 120,
     budget: 2,
     link: "https://youtu.be/dQw4w9WgXcQ?si=HfcKGydEBnTBfPPg"
   },
   {
     id: 3,
     title: 'Amogus, The saga',
-    duration: 1.60,
+    duration: 160,
     budget: 1,
     link: "https://youtu.be/dQw4w9WgXcQ?si=HfcKGydEBnTBfPPg"
   },
   
 ];
 
-// Read all the films from the menu
-router.get('/', (req, res, next) => {
-  console.log('GET /film');
-  res.json(FILMS);
-});
 
 router.get('/', (req, res) =>{
   const minimumDuration =  req.query['minimum-duration'] ? Number(req.query['minimum-duration']) : undefined;
 
-  if(minimumDuration == undefined)
-   return res.json(FILMS);
+  if(minimumDuration == undefined){
+    return res.json(FILMS);
+  }
+   
 
-  if (typeof minimumDuration !== 'number' || minimumDuration <= 0)
+  if (typeof minimumDuration !== 'number' || minimumDuration <= 0){
     return res.sendStatus(400);
+  }
 
-    const filmsReachingMinimumDuration = FILMS.filter(
-      (film) => film.duration >= minimumDuration
-    );
-    return res.json(filmsReachingMinimumDuration)
+  const filmsReachingMinimumDuration = FILMS.filter(
+    (film) => film.duration >= minimumDuration
+  );
+  
+
+  
+    
+  
+
+  const orderByTitle =
+  req?.query?.order?.includes('title')
+    ? req.query.order
+    : undefined;
+  let orderedFilms;
+  console.log(`order by ${orderByTitle ?? 'not requested'}`);
+  if (orderByTitle)
+    orderedFilms = [...FILMS].sort((a, b) => a.title.localeCompare(b.title));
+  if (orderByTitle === '-title') orderedFilms = orderedFilms.reverse();
+
+  console.log('GET /films');
+  res.json(orderedFilms ?? FILMS);
+
+  return res.json(filmsReachingMinimumDuration)
 });
 
 // Read the film identified by an id in the menu
@@ -80,30 +97,15 @@ router.post('/', (req, res) => {
     budget: budget,
     link: link,
   };
+  
 
   FILMS.push(newFilm);
+
+  console.log(FILMS);
 
   res.json(newFilm);
 });
 
-/* Read all the pizzas from the menu
-   GET /films?order=title : ascending order by title
-   GET /films?order=-title : descending order by title
-*/
-router.get('/', (req, res, next) => {
-  const orderByTitle =
-    req?.query?.order?.includes('title')
-      ? req.query.order
-      : undefined;
-  let orderedFilms;
-  console.log(`order by ${orderByTitle ?? 'not requested'}`);
-  if (orderByTitle)
-    orderedFilms = [...FILMS].sort((a, b) => a.title.localeCompare(b.title));
-  if (orderByTitle === '-title') orderedFilms = orderedFilms.reverse();
-
-  console.log('GET /films');
-  res.json(orderedFilms ?? FILMS);
-});
 
 
 
